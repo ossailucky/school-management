e014aa9be395f7e2e6f6471a25f6f0b9ec8c1516
Ossai Lucky <luckyblaqy@gmail.com>
2023-03-20 01:54:27 +0100

    create userParam decorator to get authenicated user data

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -9,6 +9,7 @@ import { GqlAuthGuard } from 'src/auth/guards/jwt-auth-guard';
 import { hasRoles } from 'src/auth/decorators/roles.decorators';
 import { RolesGuard } from 'src/auth/guards/roles.guards';
 import { Role } from './entities/user.entity';
+import { CurrentUser } from 'src/auth/decorators/currentuser.decorator';
 
 @Resolver(() => UserType)
 export class UserResolver {
@@ -26,9 +27,10 @@ export class UserResolver {
     return this.userService.getAllUsers();
   }
 
-  @Query(() => UserType, { name: 'user' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.userService.findOne(id);
+  @Query(returns => UserType, { name: 'user' })
+  @UseGuards(GqlAuthGuard)
+  findOne(@CurrentUser() user:User) {
+    return user;
   }
 
   
