4649b733531b5d1e4ff53fd7743984f17b16a296
Ossai Lucky <luckyblaqy@gmail.com>
2023-12-04 22:45:29 +0100

    add guard protection for the create subject and get all subject resolver

--- src/subject/subject.resolver.ts
+++ src/subject/subject.resolver.ts
@@ -4,16 +4,25 @@ import { Subject } from './entities/subject.entity';
 import { CreateSubjectInput } from './dto/create-subject.input';
 import { UpdateSubjectInput } from './dto/update-subject.input';
 import { SubjectType } from './entities/subject.type';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from 'src/auth/guards/jwt-auth-guard';
+import { RolesGuard } from 'src/auth/guards/roles.guards';
+import { hasRoles } from 'src/auth/decorators/roles.decorators';
+import { Role } from 'src/user/entities/user.entity';
 
 @Resolver(() => SubjectType)
 export class SubjectResolver {
   constructor(private readonly subjectService: SubjectService) {}
 
+  @hasRoles(Role.ADMIN, Role.SECRETARY)
+  @UseGuards(GqlAuthGuard, RolesGuard)
   @Mutation(returns => SubjectType)
   async createSubject(@Args('createSubjectInput') createSubjectInput: CreateSubjectInput) {
     return this.subjectService.createSubject(createSubjectInput);
   }
 
+  @hasRoles(Role.ADMIN, Role.SECRETARY)
+  @UseGuards(GqlAuthGuard, RolesGuard)
   @Query(returns => [SubjectType], { name: 'subjects' })
   findAll() {
     return this.subjectService.getAllSubjects();
