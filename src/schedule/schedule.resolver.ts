c30a9a28b984cd33a0bfc906e181d555a12e6cc3
Ossai Lucky <luckyblaqy@gmail.com>
2024-01-06 16:01:48 +0100

    add protection to create schedule resolver

--- src/schedule/schedule.resolver.ts
+++ src/schedule/schedule.resolver.ts
@@ -4,11 +4,18 @@ import { Schedule } from './entities/schedule.entity';
 import { CreateScheduleInput } from './dto/create-schedule.input';
 import { UpdateScheduleInput } from './dto/update-schedule.input';
 import { ScheduleType } from './entities/schedule.type';
+import { hasRoles } from 'src/auth/decorators/roles.decorators';
+import { Role } from 'src/user/entities/user.entity';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from 'src/auth/guards/jwt-auth-guard';
+import { RolesGuard } from 'src/auth/guards/roles.guards';
 
 @Resolver(() => Schedule)
 export class ScheduleResolver {
   constructor(private readonly scheduleService: ScheduleService) {}
 
+  @hasRoles(Role.ADMIN, Role.SECRETARY)
+  @UseGuards(GqlAuthGuard, RolesGuard)
   @Mutation(returns => ScheduleType)
   createSchedule(@Args('createScheduleInput') createScheduleInput: CreateScheduleInput) {
     return this.scheduleService.createSchedule(createScheduleInput);
